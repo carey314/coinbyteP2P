ef4185b13e46da6febbafa78969e6ec28d497bde
Kris-Wang <xmqywx@gmail.com>
2023-08-30 12:03:39 +0800

    修复

--- src/api/blog.ts
+++ src/api/blog.ts
@@ -2,8 +2,8 @@ import {default as httpAdmin} from '../utils/httpAdminApi';
 import { GetBlogs } from '../models/blog';
 async function getBlogs(data?: any){
     let configData = {
-        pageNumber: 1,
-        pageSize: 1000
+        page: 1,
+        limit: 1000
     }
     if(data) {
         configData = Object.assign(configData, data);
